403ac4ae291f74c43471100eb4d8d6e75eec8865
slav4ocom <slav4o_1@abv.bg>
2022-01-07 00:20:16 +0300

    added search functionality and search with pressing enter key

--- app.js
+++ app.js
@@ -24,41 +24,51 @@ function FillTable() {
 
 }
 
+//var data;
+
 function Search() {
-    var result;
+    var container = document.getElementById('resultsContainer');
+    container.innerHTML = "";
     var searchString = document.getElementById('searchField').value;
     ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=" + searchString + "&f=json", function (text) {
-        result = JSON.parse(text);
-    })
-
-    //FillTable();
-    CreateTable();
-}
-
-function CreateTable(container) {
-
-    readTextFile("/roomdata.json", function (text) {
-        data = JSON.parse(text);
+    //ReadTextFile("/roomdata.json", function (text) {
+        var data = JSON.parse(text).candidates;
         var myTable = document.getElementById('resultsContainer');
-
+        var id = 1;
+        console.log(data);
         data.forEach(element => {
             var tableRow = document.createElement('tr');
             var tableCol1 = document.createElement('td');
             var tableCol2 = document.createElement('td');
-            var tableCol3 = document.createElement('td');
-            AddDropdownButton(tableCol3, element.conditioner);
-            var tableCol4 = document.createElement('td');
+            //var tableCol3 = document.createElement('td');
+            //AddDropdownButton(tableCol3, element.conditioner);
+            //var tableCol4 = document.createElement('td');
             tableRow.appendChild(tableCol1);
             tableRow.appendChild(tableCol2);
-            tableRow.appendChild(tableCol3);
-            tableCol1.innerHTML = element.id;
-            tableCol2.innerHTML = element.room;
-            tableCol4.innerHTML = element.repeater;
-            tableRow.appendChild(tableCol4);
+            //tableRow.appendChild(tableCol3);
+            tableCol1.innerHTML = id;
+            tableCol2.innerHTML = element.address;
+            //tableCol4.innerHTML = element.repeater;
+            //tableRow.appendChild(tableCol4);
             container.appendChild(tableRow);
-            //console.log(element.id);
-            //console.log(element.conditioner);
-            //console.log(element.repeater);
+            console.log(element.address);
+            id++;
         });
 
     })
+
+}
+
+// Get the input field
+var input = document.getElementById("searchField");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("magnifier").click();
+  }
+});
