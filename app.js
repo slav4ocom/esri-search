78ad4e9eede38ce13eca3be694d27d9193d61781
slav4ocom <slav4o_1@abv.bg>
2022-01-07 02:31:12 +0300

    added suggestions functionality within search menu

--- app.js
+++ app.js
@@ -10,28 +10,43 @@ function ReadTextFile(file, callback) {
     rawFile.send(null);
 }
 
-function FillTable() {
-    var containter = document.getElementById('resultsContainer');
-    var tableRow = document.createElement('tr');
-    var addressNumberElement = document.createElement('td');
-    var addressElement = document.createElement('td');
-
-    containter.innerHTML = "";
-    addressElement.innerHTML = "Това е адрес 1";
-    tableRow.appendChild(addressNumberElement);
-    tableRow.appendChild(addressElement);
-    containter.appendChild(tableRow);
-
+function Suggest() {
+    var searchString = document.getElementById('searchField').value;
+    ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&text=" + searchString, function (text) {
+        var data = JSON.parse(text);
+        console.log(data);
+    })
 }
 
-//var data;
+
+
+function Suggest() {
+    var container = document.getElementById('dropdownContainer');
+    var searchString = document.getElementById('searchField').value;
+    var data;
+    ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&text=" + searchString, function (text) {
+        data = JSON.parse(text);
+        console.log("dropdown content:");
+        console.log(data);
+        //const suggestions = ["Горна Оряховица", "Бов", "Казичене", "Синдел"];
+        var suggestions = data.suggestions;
+        container.innerHTML = "";
+        suggestions.forEach(element => {
+            var suggestion = document.createElement('a');
+            suggestion.innerHTML = element.text;
+            container.appendChild(suggestion);
+            console.log(element);
+        });
+    });
+}
+
+//AddDropdownContent(document.getElementById('dropdownContainer'));
 
 function Search() {
     var container = document.getElementById('resultsContainer');
     container.innerHTML = "";
     var searchString = document.getElementById('searchField').value;
     ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=" + searchString + "&f=json", function (text) {
-    //ReadTextFile("/roomdata.json", function (text) {
         var data = JSON.parse(text).candidates;
         var myTable = document.getElementById('resultsContainer');
         var id = 1;
@@ -40,18 +55,15 @@ function Search() {
             var tableRow = document.createElement('tr');
             var tableCol1 = document.createElement('td');
             var tableCol2 = document.createElement('td');
-            //var tableCol3 = document.createElement('td');
-            //AddDropdownButton(tableCol3, element.conditioner);
-            //var tableCol4 = document.createElement('td');
+
             tableRow.appendChild(tableCol1);
             tableRow.appendChild(tableCol2);
-            //tableRow.appendChild(tableCol3);
+
             tableCol1.innerHTML = id;
             tableCol2.innerHTML = element.address;
-            //tableCol4.innerHTML = element.repeater;
-            //tableRow.appendChild(tableCol4);
+
             container.appendChild(tableRow);
-            console.log(element.address);
+            //console.log(element.address);
             id++;
         });
 
@@ -63,12 +75,14 @@ function Search() {
 var input = document.getElementById("searchField");
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("magnifier").click();
-  }
+input.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("magnifier").click();
+    }
 });
+
+//AddDropdownButton(document.getElementById('suggestionsButton'), "suggestions");
