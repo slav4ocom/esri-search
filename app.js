20a6a1295a3df5b2aedef8f1ced0c96a6088f780
slav4ocom <slav4o_1@abv.bg>
2022-01-07 21:02:15 +0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var searchField = document.getElementById("searchField");
+var suggestionsLength = 0;
+var dropdownIndex = -1;
+var selectedSuggestion;
+var lastSearchString = "";
+var dropdownContainer = document.getElementById('dropdownContainer');
+//var body = document.getElementById("body");
+
+
 function ReadTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -10,7 +19,6 @@ function ReadTextFile(file, callback) {
     rawFile.send(null);
 }
 
-var suggestionsLength = 0;
 
 function GetSuggestions() {
     var container = document.getElementById('dropdownContainer');
@@ -46,7 +54,7 @@ function GetSuggestions() {
 function Search(searchString) {
     var container = document.getElementById('resultsContainer');
     container.innerHTML = "";
-    //var searchString = document.getElementById('searchField').value;
+    lastSearchString = searchString;
     ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=" + searchString + "&f=json", function (text) {
         var data = JSON.parse(text).candidates;
         var myTable = document.getElementById('resultsContainer');
@@ -72,103 +80,86 @@ function Search(searchString) {
 
 }
 
-
-// Get the input field
-var searchField = document.getElementById("searchField");
-
-// Execute a function when the user releases a key on the keyboard
-searchField.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("magnifier").click();
-    }
-});
-
-var dropdownIndex = -1;
-var selectedSuggestion;
-
-searchField.addEventListener("keyup", function (event) {
-    if (event.keyCode == 38) {
-        //console.log("UP");
-        dropdownContainer.setAttribute("style", "display: block;");
-        dropdownIndex--;
-        if (dropdownIndex < 0) {
-            dropdownIndex = suggestionsLength - 1;
+function AddListeners() {
+    searchField.addEventListener("keyup", function (event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("magnifier").click();
         }
+    });
+
+
+    searchField.addEventListener("keyup", function (event) {
+        if (event.keyCode == 38) {
+            dropdownContainer.setAttribute("style", "display: block;");
+            dropdownIndex--;
+            if (dropdownIndex < 0) {
+                dropdownIndex = suggestionsLength - 1;
+            }
+            if (selectedSuggestion != undefined) {
+                selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
+            }
+            selectedSuggestion = dropdownContainer.getElementsByTagName("a")[dropdownIndex];
+            if (selectedSuggestion != undefined) {
+                selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
+                searchField.value = selectedSuggestion.innerHTML;
+            }
+
+        }
+        else if (event.keyCode == 40) {
+            dropdownContainer.setAttribute("style", "display: block;");
+            dropdownIndex++;
+            if (dropdownIndex > suggestionsLength - 1) {
+                dropdownIndex = 0;
+            }
+            if (selectedSuggestion != undefined) {
+                selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
+            }
+            selectedSuggestion = dropdownContainer.getElementsByTagName("a")[dropdownIndex];
+            if (selectedSuggestion != undefined) {
+                selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
+                searchField.value = selectedSuggestion.innerHTML;
+            }
+
+        }
+    });
+
+    searchField.addEventListener("click", function (event) {
+        dropdownContainer.setAttribute("style", "display: block;");
+    });
+
+    document.addEventListener("keyup", function (event) {
+        if (event.keyCode == 27) {
+            dropdownContainer.setAttribute("style", "");
+            searchField.value = lastSearchString;
+        }
+    });
+
+
+    dropdownContainer.addEventListener("click", function (event) {
+        var pointed = event.path[0].innerHTML;
+        searchField.value = pointed;
+        Search(pointed);
+    });
+
+    dropdownContainer.addEventListener("mousemove", function (event) {
         if (selectedSuggestion != undefined) {
             selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
         }
-        selectedSuggestion = dropdownContainer.getElementsByTagName("a")[dropdownIndex];
-        if (selectedSuggestion != undefined) {
-            selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
-            searchField.value = selectedSuggestion.innerHTML;
-        }
+        var id = event.path[0].id - 1;
+        dropdownIndex = id;
+        selectedSuggestion = dropdownContainer.getElementsByTagName("a")[id];
+        selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
+    });
 
-    }
-    else if (event.keyCode == 40) {
-        //console.log("DOWN");
-        dropdownContainer.setAttribute("style", "display: block;");
-        dropdownIndex++;
-        if (dropdownIndex > suggestionsLength - 1) {
-            dropdownIndex = 0;
-        }
-        if (selectedSuggestion != undefined) {
-            selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
-        }
-        selectedSuggestion = dropdownContainer.getElementsByTagName("a")[dropdownIndex];
-        if (selectedSuggestion != undefined) {
-            selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
-            searchField.value = selectedSuggestion.innerHTML;
-        }
+    document.addEventListener("click", function (event) {
+        var clicked = event.path[0].id;
+        if (clicked != "searchField") {
+            dropdownContainer.setAttribute("style", "");
+            searchField.value = lastSearchString;
+        };
+    });
+}
 
-    }
-});
-
-searchField.addEventListener("click", function (event) {
-    dropdownContainer.setAttribute("style", "display: block;");
-});
-
-var body = document.getElementById("body");
-body.addEventListener("keyup", function (event) {
-    //console.log(event.keyCode);
-    if (event.keyCode == 27) {
-        dropdownContainer.setAttribute("style", "");
-        searchField.value = lastSearchString;
-    }
-});
-
-var lastSearchString = searchField.text;
-var dropdownContainer = document.getElementById('dropdownContainer');
-
-dropdownContainer.addEventListener("click", function (event) {
-    var pointed = event.path[0].innerHTML;
-    //console.log(pointed);
-    searchField.value = pointed;
-    //searchField.setAttribute('inner', pointed);
-    Search(pointed);
-});
-
-dropdownContainer.addEventListener("mousemove", function (event) {
-    if (selectedSuggestion != undefined) {
-        selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
-    }
-    var id = event.path[0].id - 1;
-    dropdownIndex = id;
-    selectedSuggestion = dropdownContainer.getElementsByTagName("a")[id];
-    selectedSuggestion.setAttribute('style', 'background-color: rgba(0,0,0,0.2);');
-    //console.log(id);
-    //console.log(event.path[0].innerHTML);
-});
-
-document.addEventListener("click", function (event) {
-    var clicked = event.path[0].id;
-    //console.log(clicked);
-    if (clicked != "searchField") {
-        dropdownContainer.setAttribute("style", "");
-        searchField.value = lastSearchString;
-    };
-
-});
+AddListeners();
