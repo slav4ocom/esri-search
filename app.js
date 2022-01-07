120e85a7ab757807f0956b9210dda101fb4b90f2
slav4ocom <slav4o_1@abv.bg>
2022-01-07 21:49:00 +0300

    .

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ function ReadTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
+    rawFile.timeout = 0;
     rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
             callback(rawFile.responseText);
         }
     }
-    rawFile.send(null);
+    rawFile.send();
 }
 
 
