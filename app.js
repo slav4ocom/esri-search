7f33e15d9e774d60a17a5a245de81420bf5878b4
slav4ocom <slav4o_1@abv.bg>
2022-01-07 15:40:51 +0300

    .

--- app.js
+++ app.js
@@ -10,20 +10,24 @@ function ReadTextFile(file, callback) {
     rawFile.send(null);
 }
 
+var suggestionsLength = 0;
 
-function Suggest() {
+function GetSuggestions() {
     var container = document.getElementById('dropdownContainer');
     var searchString = document.getElementById('searchField').value;
     var data;
+    var cnt = 1;
     ReadTextFile("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&text=" + searchString, function (text) {
         data = JSON.parse(text);
         //console.log("dropdown content:");
-        //console.log(data);
+        suggestionsLength = data.suggestions.length;
         //const suggestions = ["Горна Оряховица", "Бов", "Казичене", "Синдел"];
         var suggestions = data.suggestions;
         container.innerHTML = "";
         suggestions.forEach(element => {
             var suggestion = document.createElement('a');
+            suggestion.setAttribute('id', "sug" + cnt);
+            cnt++;
             suggestion.innerHTML = element.text;
             container.appendChild(suggestion);
             //console.log(element);
@@ -68,10 +72,10 @@ function SearchFromSuggestion(e) {
 }
 
 // Get the input field
-var input = document.getElementById("searchField");
+var searchField = document.getElementById("searchField");
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
+searchField.addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         // Cancel the default action, if needed
@@ -81,12 +85,47 @@ input.addEventListener("keyup", function (event) {
     }
 });
 
-var suggestionInput = document.getElementById('dropdownContainer');
+var dropdownIndex = -1;
+var selectedSuggestion;
 
-suggestionInput = document.getElementById('dropdownContainer');
+searchField.addEventListener("keyup", function (event) {
+    if (event.keyCode == 38) {
+        //console.log("UP");
+        dropdownIndex--;
+        if (dropdownIndex < 0) {
+            dropdownIndex = suggestionsLength - 1;
+        }
+        if (selectedSuggestion != undefined) {
+            selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
+        }
+    }
+    else if (event.keyCode == 40) {
+        //console.log("DOWN");
+        dropdownIndex++;
+        if (dropdownIndex > suggestionsLength - 1) {
+            dropdownIndex = 0;
+        }
+        if (selectedSuggestion != undefined) {
+            selectedSuggestion.setAttribute('style', 'background-color: #f1f1f1;');
+        }
+    }
+    //console.log(dropdownIndex);
+    selectedSuggestion = dropdownContainer.getElementsByTagName("a")[dropdownIndex];
+    if (selectedSuggestion != undefined) {
+        selectedSuggestion.setAttribute('style', 'background-color: coral;');
+        searchField.value = selectedSuggestion.innerHTML;
+    }
+    //console.log(selectedSuggestion.innerHTML);
+});
 
-suggestionInput.addEventListener("click", function (event) {
-    console.log(event.srcElement.innerText);
-    Search(event.srcElement.innerText);
+var defaultSearchString = searchField.text;
+var dropdownContainer = document.getElementById('dropdownContainer');
+
+dropdownContainer.addEventListener("click", function (event) {
+    var pointed = event.path[0].innerHTML;
+    //console.log(pointed);
+    searchField.value = pointed;
+    //searchField.setAttribute('inner', pointed);
+    Search(pointed);
 });
 
